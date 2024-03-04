d6f8b1c3021b627ee837ddb01f56bf79979a9428
Uslje <147374020+uslje26@users.noreply.github.com>
2024-03-04 23:50:22 +0100

    feat: encounter model

---
+++ encounters-api/model/encounter.go
@@ -0,0 +1,30 @@
+package model
+
+type EncounterStatus int
+
+const (
+	Draft EncounterStatus = iota
+	Archived
+	Published
+)
+
+type EncounterType int
+
+const (
+	Social EncounterType = iota
+	Location
+	Misc
+)
+
+type Encounter struct {
+	// AuthorID
+	ID          uint64          `json:"id"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	XP          uint64          `json:"XP"`
+	Status      EncounterStatus `json:"status"`
+	Type        EncounterType   `json:"type"`
+	Longitude   float64         `json:"longitude"`
+	Latitude    float64         `json:"latitude"`
+	// List of changes
+}
