abcd5110c59a79efe5f7c1b3599a59ee6f4075b3
Uslje <147374020+uslje26@users.noreply.github.com>
2024-03-04 20:55:30 +0100

    feat: logger middleware

--- encounters/main.go
+++ encounters/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -10,6 +11,7 @@ import (
 func main() {
 
 	router := mux.NewRouter()
+	router.Use(loggerMiddleware)
 	router.HandleFunc("/hello", basicHandler).Methods("GET")
 
 	server := &http.Server{
@@ -24,4 +26,22 @@ func main() {
 
 func basicHandler(resp http.ResponseWriter, req *http.Request) {
 	resp.Write([]byte("Hello world"))
+}
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+
+		// Log the request
+		fmt.Printf(
+			"[%s] %s %s %v\n",
+			r.Method,
+			r.RequestURI,
+			r.RemoteAddr,
+			time.Since(start),
+		)
+	})
 }
